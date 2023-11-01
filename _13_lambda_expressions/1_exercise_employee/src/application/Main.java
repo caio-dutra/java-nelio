f66b98238ce637e481938328678d2e73871ed9f1
Caio Dutra <caiodutrasnt@gmail.com>
2023-10-31 21:55:44 -0300

    lambda exercise

---
+++ _13_lambda_expressions/1_exercise_employee/src/application/Main.java
@@ -0,0 +1,61 @@
+package application;
+
+import java.io.BufferedReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employee;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employee> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+			}
+			
+			System.out.print("Enter salary: ");
+			Double salary = sc.nextDouble();
+			
+			List<String> emails = list.stream()
+					.filter(p -> p.getSalary() > salary)
+					.map(p -> p.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
+			
+			System.out.println("Email of people whose salary is more than 2000.00:");
+			emails.forEach(System.out::println);
+			
+			double avg = list.stream()
+					.filter(p -> p.getName().charAt(0) == 'M')
+					.map(p -> p.getSalary())
+					.reduce(0.0, (x,y) -> x + y) / list.size();
+			
+			System.out.println("Sum of salary of people whose name starts with 'M': " + avg);
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+}
